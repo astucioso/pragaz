46217e9251b6ba6185efa707f210494725495592
astucioso <150879600+astucioso@users.noreply.github.com>
2024-11-13 09:07:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,39 +34,5 @@ document.getElementById('profile')?.addEventListener('mouseleave', function() {
         profile.style.transform = 'rotateX(0deg) rotateY(0deg)'; 
     }
 });
-const express = require('express');
-const fetch = require('node-fetch');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-const webhookUrl = 'https://discord.com/api/webhooks/1306224276323438612/RFZXjiZqUcr_Aoo3NAzZNWVEtbYShSyNOBTbEycjAH5hT4Vb_TqGWaFsr6ZHtj0qPnU-';
-
-app.get('/', (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    const message = `Novo acesso ao site! IP do usuário: ${ip}, Página: ${req.originalUrl}, Data: ${new Date().toLocaleString()}`;
-
-    sendToDiscord(message);
-
-    res.send('Bem-vindo ao site!');
-});
-
-function sendToDiscord(message) {
-    const payload = {
-        content: message,
-        username: 'Notificador do Site'
-    };
-
-    fetch(webhookUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(payload)
-    }).then(response => response.json())
-      .then(data => console.log('Mensagem enviada para o Discord', data))
-      .catch(error => console.error('Erro ao enviar a mensagem para o Discord', error));
-}
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
 
